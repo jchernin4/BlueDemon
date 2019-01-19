f041507de154e1cf4972bbba377dfb1b7381e195
Jeremy N Chernin <203327@kcsd.org>
2019-01-19 15:35:13 -0500

    Added default board 366434

--- mybot.js
+++ mybot.js
@@ -74,6 +74,6 @@ function make_move() {
 // Optionally include this function if you'd like to always reset to a 
 // certain board number/layout. This is useful for repeatedly testing your
 // bot(s) against known positions. 
-//function default_board_number() {
-//    return 123;
-//}
+function default_board_number() {
+    return 366434;
+}
