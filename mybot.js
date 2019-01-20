2cbd48c5488bf5eac32ea9030faeab462c23f083
LukeS26 <thewarriorls@outlook.com>
2019-01-20 14:01:48 -0500

    Push to check

--- mybot.js
+++ mybot.js
@@ -55,53 +55,42 @@ function make_move() {
 
    console.log(FruitAvailableFinal);
    
-   Count = 0;
+   Count = 1;
 
    while(Count < (WIDTH * HEIGHT))
    {
-       DistanceX = (Math.abs((get_my_x() - FruitPositionsX[Count])));
-       DistanceY = (Math.abs((get_my_y() - FruitPositionsY[Count])));
+       DistanceX = (Math.abs((get_my_x() - FruitPositionsX[Count -1])));
+       DistanceY = (Math.abs((get_my_y() - FruitPositionsY[Count - 1])));
        FruitDistances.push((DistanceX + DistanceY + 0.01));
-       DistanceX = (Math.abs((get_opponent_x() - FruitPositionsX[Count])));
-       DistanceY = (Math.abs((get_opponent_y() - FruitPositionsY[Count])));
+       DistanceX = (Math.abs((get_opponent_x() - FruitPositionsX[Count - 1])));
+       DistanceY = (Math.abs((get_opponent_y() - FruitPositionsY[Count - 1])));
        EnemyFruitDistances.push((DistanceX + DistanceY + 0.01));
        Count = Count + 1;
    }
+   FruitDistances.push((DistanceX + DistanceY + 1.01));
    console.log("<=========== Logging FruitDistances ===========>");
    console.log(FruitDistances);
 
    Count = 0;
 
    while(Count < FruitDistances.length){
-       if(board[FruitPositionsX[Count]],[FruitPositionsY[Count]] > 0 && get_my_item_count(board[FruitPositionsX[Count]][FruitPositionsY[Count]]) <= (.5 * get_total_item_count(board[FruitPositionsX[Count]][FruitPositionsY[Count]])))
-       {
-          
-         ItemWeight.push(1/(.5 * (get_total_item_count(FruitAvailableFinal[Count])) * (5/(FruitDistances[Count]))));
-         ItemWeightVar = ItemWeight[Count];
-         if(EnemyFruitDistances[Count]/FruitDistances[Count] < 1 || isNaN(EnemyFruitDistances[Count]/FruitDistances[Count]))
-         {
+        ItemWeight.push((5/FruitDistances[Count]) * (3 * (get_my_item_count(board[FruitPositionsX[Count]][FruitPositionsY[Count]])/get_total_item_count(board[FruitPositionsX[Count]],[FruitPositionsY[Count]]))));
+        ItemWeightVar = ItemWeight[Count];
+        if(EnemyFruitDistances[Count]/FruitDistances[Count] < 1 || isNaN(EnemyFruitDistances[Count]/FruitDistances[Count]))
+        {
             ItemWeight.splice(Count);
             ItemWeight.push(ItemWeightVar * 0.5);
-         }   
-         else
-         {
-            ItemWeight.splice(Count);
-            ItemWeight.push(ItemWeightVar * 1.5);
-         }
-           if(ItemWeight[Count] == 0)
-            {
-                ItemWeight.splice(Count);
-                ItemWeight.push(0.05);
-            }
-       }
-       else{
-        ItemWeight.push(0);
-       } 
-       if(isNaN(ItemWeight[Count]))
-       {
-        ItemWeight.splice(Count);
-        ItemWeight.push(-1);
-       }
+        }   
+        else
+        {
+         ItemWeight.splice(Count);
+         ItemWeight.push(ItemWeightVar * 1.5);
+        }
+        //if(isNaN(ItemWeight[Count]))
+        //{
+         //ItemWeight.splice(Count);
+         //ItemWeight.push(0);
+        //}
        Count = Count + 1;
    }
 
@@ -137,6 +126,6 @@ function make_move() {
 // Optionally include this function if you'd like to always reset to a 
 // certain board number/layout. This is useful for repeatedly testing your
 // bot(s) against known positions. 
-//function default_board_number() {
-//    return 366434;
-//}
+function default_board_number() {
+    return 62809;
+}
