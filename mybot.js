525a753587513374f5901807f80ed1b11203e192
LukeS26 <thewarriorls@outlook.com>
2019-01-20 14:46:55 -0500

    Added PASS

--- mybot.js
+++ mybot.js
@@ -121,6 +121,9 @@ function make_move() {
    if (CurrentGoalY > get_my_y()) return SOUTH;
    if (CurrentGoalY < get_my_y()) return NORTH;
 
+   return PASS;
+
+    }
    /* var rand = Math.random() * 4; // TO DO: Point towards fruit instead of random path generation
 
    if (rand < 1) return NORTH;
@@ -129,10 +132,9 @@ function make_move() {
    if (rand < 4) return WEST; */
 
    //return PASS;
-    }
 // Optionally include this function if you'd like to always reset to a 
 // certain board number/layout. This is useful for repeatedly testing your
 // bot(s) against known positions. 
 /* function default_board_number() {
-    return 62809;
+    return 366434;
 } */
