b392ca15de4c1909b292f643e478943b57b402cc
LukeS26 <thewarriorls@outlook.com>
2019-01-20 14:43:30 -0500

    Removed Default Map

--- mybot.js
+++ mybot.js
@@ -133,6 +133,6 @@ function make_move() {
 // Optionally include this function if you'd like to always reset to a 
 // certain board number/layout. This is useful for repeatedly testing your
 // bot(s) against known positions. 
-function default_board_number() {
+/* function default_board_number() {
     return 62809;
-}
+} */
